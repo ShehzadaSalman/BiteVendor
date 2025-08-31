34fc55eef617b81511923e836f7efba1eef5643c
aaozgull <aaozgull@gmail.com>
2025-08-31 13:17:35 +0500

    initial Commit

--- App.js
+++ App.js
@@ -4,12 +4,9 @@
  *
  * @format
  */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View , Text} from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
@@ -24,14 +21,11 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={{color:'red',
+    textAlign:'center'}}>Hi</Text>
     </View>
   );
 }
@@ -39,6 +33,7 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:'center',
   },
 });
 
